53f0297ba3d6c4d239e69d379739966c4da726db
Hardthof <Philippe.Cheray@gmail.com>
2021-01-01 19:10:11 +0100

    Add Day17 part2

--- AdventDay17.py
+++ AdventDay17.py
@@ -2,66 +2,63 @@ import cProfile
 from collections import namedtuple
 from itertools import product
 
-# create all non existent neighbors of active cubes
-def expandUniverse(universe):
-    newUniverse = {}
-    for coord in universe:
-        if universe[coord]:
-            for offset in neighbors:
-                newUniverse[addCoord(coord, offset)] = False
-    return newUniverse
+class Universe:
+    def __init__(self, n, file):
+        self.n = n
+        self.file = file  
+        self.universe = self.populateUniverse(file)
+        self.neighbors = set(Coordinate(*offset) for offset in product([-1, 0, 1], repeat = n) if offset != (0,) * n)
 
-# count neighbors till a max of [lim]
-def countActiveNeighbours(universe, coord, lim=4):
-    count = 0
-    for offset in neighbors:
-        if universe.get(addCoord(coord, offset)): 
-                count += 1
-                if count >= lim:
-                    break
+    # create all non existent neighbors of active cubes
+    def expandUniverse(self, universe):
+        newUniverse = {}
+        for coord in universe:
+            if universe[coord]:
+                for offset in self.neighbors:
+                    newUniverse[self.addCoord(coord, offset)] = False
+        return newUniverse
 
-    # is there a way to find potential hotspots from active universes?
-        # for now limit searchspace to cube minX,minY,minZ - maxX,maxY,maxZ
-        # r Trees, geohash, kd tree
+    # count neighbors till a max of [lim]
+    def countActiveNeighbours(self, coord, lim=4):
+        count = 0
+        for offset in self.neighbors:
+            if self.universe.get(self.addCoord(coord, offset)): 
+                    count += 1
+                    if count >= lim:
+                        break
+        return count
 
-    #print(coord, 'has', count, 'active neighbors: ')
-    return count
+    def transition(self, active, activeNeighbors):
+        return activeNeighbors == 3 or (activeNeighbors == 2 and active)
 
-def transition(active, activeNeighbors):
-    return activeNeighbors == 3 or (activeNeighbors == 2 and active)
+    def cycleUniverse(self):
+        newUniverse = self.expandUniverse(self.universe)
+        for cube in newUniverse:
+            newUniverse[cube] = self.transition(self.universe.get(cube, False), self.countActiveNeighbours(cube))
+        self.universe = newUniverse
 
-def cycleUniverse(knownUniverse):
-    newUniverse = expandUniverse(knownUniverse)
-    for cube in newUniverse:
-        newUniverse[cube] = transition(knownUniverse.get(cube, False), countActiveNeighbours(knownUniverse, cube))
-    return newUniverse
+    def populateUniverse(self, file):
+        universe = {}
+        with open(file) as f:
+            for y,l in enumerate(f.readlines()):
+                for x in range(len(l.rstrip('\n'))):
+                    universe[Coordinate(x,y)] = (l[x] == '#')
+        return universe
 
-def populateUniverse(file):
-    universe = {}
-    with open(file) as f:
-        for y,l in enumerate(f.readlines()):
-            for x in range(len(l.rstrip('\n'))):
-                universe[Coordinate(x,y,0)] = (l[x] == '#')
-    return universe
+    def addCoord(self, coord, offset):
+        return Coordinate(*map(sum, zip(coord, offset)))
 
-def addCoord(coord, offset):
-    return Coordinate(*map(sum, zip(coord, offset)))
+    def start(self, n):
+        for _ in range(n):
+            self.cycleUniverse()
 
+        print(sum(map((True).__eq__, self.universe.values())))
 
-def start(n):
-    u = populateUniverse('AdventOfCode/Day17.txt')
+Coordinate = namedtuple('Coordinate', ['x', 'y', 'z'], defaults=(0,0,0))
+P1 = Universe(n = 3, file = 'AdventOfCode/Day17.txt')
+cProfile.run('P1.start(6)')
 
-    for _ in range(6):
-        u = cycleUniverse(u)
-
-    print(sum(map((True).__eq__, u.values())))
-
-Coordinate = namedtuple('Coordinate',  ['x', 'y', 'z'])
-neighbors = set(Coordinate(*offset) for offset in product([-1, 0, 1], repeat = 3) if offset != (0,0,0))
-n = 3
-cProfile.run('start(n)')
-
-Coordinate = namedtuple('Coordinate',  ['x', 'y', 'z', 'w'], defaults=(None))
-neighbors = set(Coordinate(*offset) for offset in product([-1, 0, 1], repeat = 4) if offset != (0,0,0,0))
-#cProfile.run('start(n)')
+Coordinate = namedtuple('Coordinate',  ['x', 'y', 'z', 'w'], defaults=(0,0,0,0))
+P2 = Universe(n = 4, file = 'AdventOfCode/Day17.txt')
+cProfile.run('P2.start(6)')
 
